36c010a39493806e56bc19fb60d09ca0a437e234
DJMIKE20 <m.zimm20@gmail.com>
2022-11-14 14:37:33 -0500

    added dependencies and Concurrently, to install packages in respective locations

---
+++ server/routes/htmlRoutes.js
@@ -0,0 +1,9 @@
+// import path
+const path = require('path');
+
+// export function and file
+module.exports = function(app) {
+    app.get('/', function(req, res){
+        res.sendFile(path.join(__dirname, '../../client/index.html'));
+    });
+};
---
+++ server/server.js
@@ -0,0 +1,19 @@
+// import express.js
+const express = require('express');
+
+// initialize port 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// use server
+app.use(express.static('../client/'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// use HTML route in htmlRoutes
+require('./routes/htmlRoutes.js')(app);
+
+// listen to server
+app.listen(PORT, function() {
+    console.log(`Now listening on port: ${PORT}`);
+});
