76cf1ccf74d1a387274020892f77e1dcfc38ad5d
DJMIKE20 <m.zimm20@gmail.com>
2022-11-15 12:50:05 -0500

    added webpacks and heroku staging setup

---
+++ client/src/js/index.js
@@ -0,0 +1,39 @@
+// import loadash, js files and images
+import _ from 'lodash';
+
+// import css styles 
+import '../css/index.css';
+
+// impoer modules 
+import  "./form";
+import "./submit";
+
+// import images 
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
+import buzzCity from '../images/buzz-city.png';
+
+// import bootstrap npm modules 
+import { Tooltip, Toast, Popover } from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// add images on load
+window.addEventListener('load', function () {
+  document.getElementById('logo').src = buzzCity;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
+});
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    return element;
+  }
+  
+  document.body.appendChild(component());
+
+  
--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -4,6 +4,6 @@ const path = require('path');
 // export function and file
 module.exports = function(app) {
     app.get('/', function(req, res){
-        res.sendFile(path.join(__dirname, '../../client/index.html'));
+        res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
 };
