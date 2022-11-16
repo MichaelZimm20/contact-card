a5ef78cc2ee128e7b32ff0eac7b19a0b04cc1cca
DJMIKE20 <m.zimm20@gmail.com>
2022-11-16 13:44:34 -0500

    configured install button

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -116,3 +116,23 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
   navigator.serviceWorker.register('./service-worker.js');
 })};
+
+
+// install button 
+const installBtn = document.getElementById('installBtn');
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = 'visible';
+
+    // disable install button if app already installed
+  installBtn.addEventListener('click', () => {
+    event.prompt();
+    installBtn.setAttribute('disabled', true);
+    installBtn.textContent = 'Installed!';
+    });
+  });
+
+  window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
+  });
