6d5f4bc8b3a74dfbaa9ae1a6445f18f5652f3491
DJMIKE20 <m.zimm20@gmail.com>
2022-11-16 12:26:05 -0500

    tested in heroku local

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
 
-// import html webpack plugin 
+// import html webpack and Workbox plugin 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {InjectManifest} = require('workbox-webpack-plugin');
+
+
 
 
 // export modules 
@@ -16,6 +20,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './index.html',
       title: 'Webpack Plugin',
+    }),
+    new InjectManifest({
+      swSrc: './src/sw.js',
+      swDest: 'service-worker.js'
+
     })
   ],
   module: {
